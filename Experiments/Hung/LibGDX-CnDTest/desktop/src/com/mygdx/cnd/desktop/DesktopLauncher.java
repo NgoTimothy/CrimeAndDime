37fef2bf5a4f4d2552207a95f7c1d3288578e385
Hung <hungtruong19999@gmail.com>
2019-09-15 22:35:47 -0500

    Added splash screen and Splash class to libGDX experiements

--- Experiments/Hung/LibGDX-CnDTest/desktop/src/com/mygdx/cnd/desktop/DesktopLauncher.java
+++ Experiments/Hung/LibGDX-CnDTest/desktop/src/com/mygdx/cnd/desktop/DesktopLauncher.java
@@ -1,12 +1,23 @@
 package com.mygdx.cnd.desktop;
 
+
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.cnd.CrimeandDime;
 
+
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new CrimeandDime(), config);
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CrimeandDime(), cfg);
+
+		cfg.title = "Crime & Dime";
+		cfg.useGL30 = true;
+		cfg.width = 1280;
+		cfg.height = 720;
+
+
 	}
+
+
 }
