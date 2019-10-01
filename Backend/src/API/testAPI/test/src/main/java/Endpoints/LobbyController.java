390550a25b9325dbd59a12a172b9a5d26488923c
Lucas <laknoll@iastate.edu>
2019-10-01 13:31:00 -0500

    created lobbyInfo call

--- Backend/src/API/testAPI/test/src/main/java/Endpoints/LobbyController.java
+++ Backend/src/API/testAPI/test/src/main/java/Endpoints/LobbyController.java
@@ -239,4 +239,39 @@ public class LobbyController {
         }
     }
 
+    @RequestMapping("/lobbyInfo")
+    public String lobbyInfo(@RequestParam(value = "lobbyID", defaultValue = "15481") Integer lobbyID) throws SQLException {
+        Connection con = null;
+        try {
+
+            ArrayList<Lobby> lobbies = new ArrayList<Lobby>();
+            String query = "SELECT * FROM crime_and_dime.lobby WHERE lobbyID = ?";
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(
+                    // "jdbc:mysql://localhost:3306/dime_and_crime?allowPublicKeyRetrieval=true&useSSL=false","teamTC3","TC_3CrimeAndDime");
+                    "jdbc:mysql://coms-309-tc-3.misc.iastate.edu:3306/crime_and_dime?allowPublicKeyRetrieval=true&useSSL=false", "teamTC3", "TC_3_CrimeAndDime");
+            PreparedStatement prst = con.prepareStatement(query);
+            prst.setInt(1, lobbyID);
+            ResultSet rs = prst.executeQuery();
+            while (rs.next()) {
+                Lobby lobby = new Lobby();
+                lobby.setLobbyId(rs.getInt("lobbyId"));
+                lobby.setLobbyName(rs.getString("lobbyName"));
+                lobby.setHasPassword(rs.getBoolean("hasPassword"));
+                lobby.setLobbyPassword(rs.getString("lobbyPassword"));
+                lobby.setNumberOfPlayers(rs.getInt("numberOfPlayers"));
+                lobbies.add(lobby);
+            }
+            Gson gson = new Gson();
+            String jsonString = gson.toJson(lobbies);
+            return jsonString;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        finally {
+            con.close();
+        }
+        return "Error";
+    }
+
 }
