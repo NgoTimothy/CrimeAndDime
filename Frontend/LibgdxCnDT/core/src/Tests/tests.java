ab914290094d25bdf1a0fed5993109c9af44d3f8
aswenson <aswenson@iastate.edu>
2019-10-10 21:31:13 -0500

    Added Mockito

---
+++ Frontend/LibgdxCnDT/core/src/Tests/tests.java
@@ -0,0 +1,25 @@
+package Tests;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.*;
+ 
+
+import org.junit.Test;
+import org.mockito.*;
+
+public class tests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void LobbiesTest()
+	{
+		boolean isTrue = true;
+		assertTrue(isTrue);
+	}
+
+}
