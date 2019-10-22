41738d233371b3e91803e2e58a66f506d7b357ed
Lucas <laknoll@iastate.edu>
2019-10-21 22:40:10 -0500

    Complete dummy Customer Class

--- Frontend/LibgdxCnDT/core/src/GameClasses/Customer.java
+++ Frontend/LibgdxCnDT/core/src/GameClasses/Customer.java
@@ -1,18 +1,38 @@
 package GameClasses;
 
 import java.util.ArrayList;
+import GameClasses.Inventory;
+
 
 public class Customer {
 
-    private double budget;
-    private ArrayList<Item> preferences; // must be within budget
+    private Item item;
+    boolean hasBought;
+    //private double budget;
+    //private ArrayList<Item> preferences; // must be within budget
 
     public Customer(){
-        preferences = new ArrayList<Item>();
-        budget = 0;
+        Item item = new Item();
+        hasBought = false;
     }
 
-    public void buyItems(){
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public void setHasBought(boolean hasBought) {
+        this.hasBought = hasBought;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public boolean isHasBought() {
+        return hasBought;
+    }
+
+    public void buyItems(Inventory inventory){
         /*
         loop through list of items and check which store has preferred items
 
@@ -23,42 +43,47 @@ public class Customer {
 
         repeat process if able to purchase another item
          */
-    }
-    public void setBudget(){
-        budget = Math.floor(Math.random() * 495) +5;
-    }
-
-    public void setPreferences(double budget){
-        /*
-            -loops through all items
-            -randomly chooses 5-10 items that are less than or equal to budget
-        */
-    }
-
-    public double getBudget() {
-        return budget;
-    }
-
-    public ArrayList<Item> getPreferences() {
-        return preferences;
-    }
-
-    public void updatePreferences(double budget, ArrayList<Item> preferences, Item item){
-        /*
-            -loop through current preferences
-            -remove item purchased
-            -remove items they can no longer afford
-        */
+        if(hasBought == false) {
+            inventory.removeItem(item);
+            hasBought = true;
+        }
 
     }
-
-    public void subtractBudgjet(double budget, double price){
-        budget = budget - price;
-    }
-
-    public void removeItemFromStock(/*?*/){
-        //should be in inventory class
-    }
+//    public void setBudget(){
+//        budget = Math.floor(Math.random() * 495) +5;
+//    }
+//
+//    public void setPreferences(double budget){
+//        /*
+//            -loops through all items
+//            -randomly chooses 5-10 items that are less than or equal to budget
+//        */
+//    }
+//
+//    public double getBudget() {
+//        return budget;
+//    }
+//
+//    public ArrayList<Item> getPreferences() {
+//        return preferences;
+//    }
+//
+//    public void updatePreferences(double budget, ArrayList<Item> preferences, Item item){
+//        /*
+//            -loop through current preferences
+//            -remove item purchased
+//            -remove items they can no longer afford
+//        */
+//
+//    }
+//
+//    public void subtractBudget(double budget, double price){
+//        budget = budget - price;
+//    }
+//
+//    public void removeItemFromStock(/*?*/){
+//        //should be in inventory class
+//    }
 
 
 }
