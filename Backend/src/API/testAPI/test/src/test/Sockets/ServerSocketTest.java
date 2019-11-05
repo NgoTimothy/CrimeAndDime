b022c2cc08f078856f85fe150339132d54784cd0
Timothy D Ngo <37946614+NgoTimothy@users.noreply.github.com>
2019-11-05 10:06:24 -0600

    Add tests for server sockets

---
+++ Backend/src/API/testAPI/test/src/test/Sockets/ServerSocketTest.java
@@ -0,0 +1,13 @@
+package Sockets;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ServerSocketTest {
+
+    @Test
+    public void trueTest() {
+        assertEquals(1,1);
+    }
+}
