65d77d618489918eb78519bddfa9b79128951927
Lucas <laknoll@iastate.edu>
2019-11-19 13:56:39 -0600

    Clean up API code

--- Backend/src/API/testAPI/test/src/main/java/Endpoints/LoginController.java
+++ Backend/src/API/testAPI/test/src/main/java/Endpoints/LoginController.java
@@ -13,21 +13,15 @@ public class LoginController {
     public String register(@RequestParam(value = "username", defaultValue = "Player 1") String username ,@RequestParam(value = "password", defaultValue = "1234") String password) throws SQLException {
         Connection con = null;
         try {
-
-            //String query = "INSERT INTO lobby_group(username, lobbyID) VALUES(?, ?)";
+            
             String query2 = "INSERT INTO login(username, password) VALUES(?, ?)";
             Class.forName("com.mysql.jdbc.Driver");
             con = DriverManager.getConnection(
-                    // "jdbc:mysql://localhost:3306/dime_and_crime?allowPublicKeyRetrieval=true&useSSL=false","teamTC3","TC_3CrimeAndDime");
                     "jdbc:mysql://coms-309-tc-3.misc.iastate.edu:3306/crime_and_dime?allowPublicKeyRetrieval=true&useSSL=false", "teamTC3", "TC_3_CrimeAndDime");
             PreparedStatement prst = con.prepareStatement(query2);
             prst.setString(1, username);
             prst.setString(2, password);
             prst.executeUpdate();
-            //prst = con.prepareStatement(query);
-            //prst.setString(1, username);
-            //prst.setInt(2, lobbyID);
-            //prst.executeUpdate();
 
             return "Accepted";
 
@@ -45,9 +39,6 @@ public class LoginController {
         Connection con = null;
         try {
 
-            String expectedU = "";
-            String expectedP = "";
-            //String query = "INSERT INTO lobby_group(username, lobbyID) VALUES(?, ?)";
             String query2 = "SELECT * FROM login WHERE username = ? AND password = ?";
             Class.forName("com.mysql.jdbc.Driver");
             con = DriverManager.getConnection(
